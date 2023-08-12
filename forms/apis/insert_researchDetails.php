4b1126b4a4d2631f741a003b8ad088e2e17c3b47
ARUNTEJAMENDA <aruntejamenda@gmail.com>
2023-08-12 14:11:26 +0530

    2:11pm research db editcomplete arun commit

--- forms/apis/insert_researchDetails.php
+++ forms/apis/insert_researchDetails.php
@@ -15,7 +15,7 @@
         // echo $r1;
         if($r1==0){
             // echo "Yes";
-            $g0 = "INSERT INTO `researchexp`(`ApplicationId`, `num_papersPublished_Nat`, `num_papersPublished_IntNat`, `papers_info`, `num_patents`, `num_booksISBN`, `num_MajResearchProjs`, `num_MinResearchProjs`, `fundsFor_MajProjs`, `fundsFor_MinProjs`, `num_projectsCompleted`, `num_reportsSubmitted`, `num_mphils_guided`, `num_phds_guided`, `num_awards_S`, `num_awards_N`, `num_awards_IntNat`, `num_SemConfPap_Nat`, `num_SemConfPap_IntNat`, `memberships_Nat`, `memberships_IntNat`) VALUES ('$id','$data[0]','$data[1]','$data[2]','$data[3]','$data[4]','$data[5]','$data[6]','$data[7]','$data[8]','$data[9]','$data[10]','$data[11]','$data[12]','$data[13]','$data[14]','$data[15]','$data[16]','$data[17]','$data[18]','$data[19]');";
+            $g0 = "INSERT INTO `researchexp`(`ApplicationId`, `num_papersPublished_Nat`, `num_papersPublished_IntNat`, `papers_info`, `num_patents`, `num_booksISBN`, `num_MajResearchProjs`, `num_MinResearchProjs`, `fundsFor_MajProjs`, `fundsFor_MinProjs`, `num_projectsCompleted`, `num_reportsSubmitted`, `num_mphils_guided`, `num_phds_guided`, `num_SemConfPap_Nat`, `num_SemConfPap_IntNat`, `memberships_Nat`, `memberships_IntNat`,`num_awards_S`, `num_awards_N`, `num_awards_IntNat`) VALUES ('$id','$data[0]','$data[1]','$data[2]','$data[3]','$data[4]','$data[5]','$data[6]','$data[7]','$data[8]','$data[9]','$data[10]','$data[11]','$data[12]','$data[13]','$data[14]','$data[15]','$data[16]','$data[17]','$data[18]','$data[19]');";
             $g = mysqli_query($con,$g0);
             if(!$g){
                 echo "err -> t_exp inserting";
@@ -29,7 +29,7 @@
                 }  
             }
         }else{
-            $g0 = "UPDATE `researchexp` SET `num_papersPublished_Nat`='$data[0]',`num_papersPublished_IntNat`='$data[1]',`papers_info`='$data[2]',`num_patents`='$data[3]',`num_booksISBN`='$data[4]',`num_MajResearchProjs`='$data[5]',`num_MinResearchProjs`='$data[6]',`fundsFor_MajProjs`='$data[7]',`fundsFor_MinProjs`='$data[8]',`num_projectsCompleted`='$data[9]',`num_reportsSubmitted`='$data[10]',`num_mphils_guided`='$data[11]',`num_phds_guided`='$data[12]',`num_awards_S`='$data[13]',`num_awards_N`='$data[14]',`num_awards_IntNat`='$data[15]',`num_SemConfPap_Nat`='$data[16]',`num_SemConfPap_IntNat`='$data[17]',`memberships_Nat`='$data[18]',`memberships_IntNat`='$data[19]' WHERE `ApplicationId`='$id';";
+            $g0 = "UPDATE `researchexp` SET `num_papersPublished_Nat`='$data[0]',`num_papersPublished_IntNat`='$data[1]',`papers_info`='$data[2]',`num_patents`='$data[3]',`num_booksISBN`='$data[4]',`num_MajResearchProjs`='$data[5]',`num_MinResearchProjs`='$data[6]',`fundsFor_MajProjs`='$data[7]',`fundsFor_MinProjs`='$data[8]',`num_projectsCompleted`='$data[9]',`num_reportsSubmitted`='$data[10]',`num_mphils_guided`='$data[11]',`num_phds_guided`='$data[12]',`num_SemConfPap_Nat`='$data[13]',`num_SemConfPap_IntNat`='$data[14]',`memberships_Nat`='$data[15]',`memberships_IntNat`='$data[16]', `num_awards_S`='$data[17]',`num_awards_N`='$data[18]',`num_awards_IntNat`='$data[19]' WHERE `ApplicationId`='$id';";
             $g = mysqli_query($con,$g0);
             if(!$g){
                 echo "err -> t_exp updating";
