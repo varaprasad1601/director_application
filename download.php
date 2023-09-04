53dba6f2dca63aa9cba3173f030628c0742841d2
Vara Prasad <45548936+varaprasad1601@users.noreply.github.com>
2023-09-05 01:16:16 +0530

    download.php

--- download.php
+++ download.php
@@ -13,169 +13,184 @@ if(isset($_GET['code']) && ($_GET['code'] =="1601/2811" || $_GET['code'] =="2811
     // Include the database connection
     include "./forms/db.php";
 
-    // Personal Details ==================================================================
     $pes_details = array();
-    $pq = mysqli_query($con, "SELECT T1.* FROM `reg_dir` T0, `personneldetails` T1 WHERE T0.`all_done` = 'tab7()' AND T0.ApplicationId = T1.ApplicationId;");
-    while ($personal_details = mysqli_fetch_assoc($pq)) {
-        $pes_res[] = $personal_details;
+    
+    $ug_details_array = array();
+    $pg_details_array = array();
+    $phd_details_array = array();
+
+    $t_details_array = array();
+
+    $p_details_array = array();
+    $a_details_array = array();
+    $s_details_array = array();
+
+    $all_details_array = array();
+
+    $registered = array();
+
+    $rg = mysqli_query($con, "SELECT * FROM `reg_dir` WHERE `all_done` = 'tab7()';");
+    $sno = 1;
+    while ($registered_data = mysqli_fetch_assoc($rg)) {
+
+
+        // Personal Details ==================================================================
+        $pq = mysqli_query($con, "SELECT * FROM `personneldetails` WHERE  ApplicationId = '{$registered_data['ApplicationId']}';");
+        $pes_res = mysqli_fetch_row($pq);
+        $personal_details[] = $pes_res;
+        // Personal Details ==================================================================
+
+
+        // Qualification Details ==================================================================
+        $ugq = mysqli_query($con, "SELECT * FROM `qualifications` WHERE ApplicationId = '{$registered_data['ApplicationId']}' AND `qualification` = 'Under Graduation';");
+        $ug_res = mysqli_fetch_row($ugq);
+        unset($ug_res[0]);
+        unset($ug_res[1]);
+        $ug_res[3] = str_replace("<>", "; ", $ug_res[3]);
+        $ug_res[4] = str_replace("<>", "; ", $ug_res[4]);
+        $ug_res[5] = str_replace("<>", "; ", $ug_res[5]);
+        $ug_details[] = $ug_res;
+
+
+        $pgq = mysqli_query($con, "SELECT * FROM `qualifications` WHERE ApplicationId = '{$registered_data['ApplicationId']}' AND `qualification` = 'Post Graduation';");
+        $pg_res = mysqli_fetch_row($pgq);
+        unset($pg_res[0]);
+        unset($pg_res[1]);
+        $pg_res[3] = str_replace("<>", "; ", $pg_res[3]);
+        $pg_res[4] = str_replace("<>", "; ", $pg_res[4]);
+        $pg_res[5] = str_replace("<>", "; ", $pg_res[5]);
+        $pg_details[] = $pg_res;
+
+        $phdq = mysqli_query($con, "SELECT * FROM `qualifications` WHERE ApplicationId = '{$registered_data['ApplicationId']}' AND `qualification` = 'Ph.D';");
+        $phd_res = mysqli_fetch_row($phdq);
+        unset($phd_res[0]);
+        unset($phd_res[1]);
+        $phd_res[3] = str_replace("<>", "; ", $phd_res[3]);
+        $phd_res[4] = str_replace("<>", "; ", $phd_res[4]);
+        $phd_res[5] = str_replace("<>", "; ", $phd_res[5]);
+        $phd_details[] = $phd_res;
+        // Qualification Details ==================================================================
+
+        // Teaching Experience ==================================================================
+        $tq = mysqli_query($con, "SELECT * FROM `teachingexp` WHERE ApplicationId = '{$registered_data['ApplicationId']}';");
+        $t_res = mysqli_fetch_row($tq);
+        unset($t_res[0]);
+        unset($t_res[1]);
+        $t_details[] = $t_res;
+        // Teaching Experience ==================================================================
+
+        // Working Experience ==================================================================
+        $profq = mysqli_query($con, "SELECT * FROM `worked_universities` WHERE ApplicationId = '{$registered_data['ApplicationId']}' AND `designation` = 'Professor';");
+        $p_res = mysqli_fetch_row($profq);
+        unset($p_res[1]);
+        unset($p_res[0]);
+        $p_res[3] = str_replace("*", "; ", $p_res[3]);
+        $p_res[4] = str_replace("*", "; ", $p_res[4]);
+        $p_res[5] = str_replace("*", "; ", $p_res[5]);
+        $p_res[6] = str_replace("*", "; ", $p_res[6]);
+        $p_details[] = $p_res;
+        
+
+        $assoq = mysqli_query($con, "SELECT * FROM `worked_universities` WHERE ApplicationId = '{$registered_data['ApplicationId']}' AND `designation` = 'Associate Professor';");
+        $a_res = mysqli_fetch_row($assoq);
+        unset($a_res[0]);
+        unset($a_res[1]);
+        $a_res[3] = str_replace("*", "; ", $a_res[3]);
+        $a_res[4] = str_replace("*", "; ", $a_res[4]);
+        $a_res[5] = str_replace("*", "; ", $a_res[5]);
+        $a_res[6] = str_replace("*", "; ", $a_res[6]);
+        $a_details[] = $a_res;
+
+        $assiq = mysqli_query($con, "SELECT * FROM `worked_universities` WHERE ApplicationId = '{$registered_data['ApplicationId']}' AND `designation` = 'Assistant Professor';");
+        $s_res = mysqli_fetch_row($assiq);
+        unset($s_res[0]);
+        unset($s_res[1]);
+        $s_res[3] = str_replace("*", "; ", $s_res[3]);
+        $s_res[4] = str_replace("*", "; ", $s_res[4]);
+        $s_res[5] = str_replace("*", "; ", $s_res[5]);
+        $s_res[6] = str_replace("*", "; ", $s_res[6]);
+        $s_details[] = $s_res;
+        // Working Experience ==================================================================
+
+        // All Details =========================================================================
+        $allq = mysqli_query($con, "SELECT T1.*, T2.*, T3.*, T4.*, T5.* FROM researchexp T1, administrativeexp T2, cases T3, other T4, applied_campus T5  WHERE T1.ApplicationId = '{$registered_data['ApplicationId']}' AND T2.ApplicationId = '{$registered_data['ApplicationId']}' AND T3.ApplicationId = '{$registered_data['ApplicationId']}' AND T4.ApplicationId = '{$registered_data['ApplicationId']}' AND T5.ApplicationId = '{$registered_data['ApplicationId']}';");
+        $all_res = mysqli_fetch_row($allq);
+        unset($all_res[0]);
+        unset($all_res[1]);
+        unset($all_res[26]);
+        unset($all_res[27]);
+        unset($all_res[31]);
+        unset($all_res[32]);
+        unset($all_res[36]);
+        unset($all_res[38]);
+        $all_res[28] = str_replace('<>', "; ", $all_res[28]);
+        $all_res[29] = str_replace('<>', "; ", $all_res[29]);
+        $all_res[30] = str_replace('<>', "; ", $all_res[30]);
+        $all_res[33] = str_replace('<>', "; ", $all_res[33]);
+        $all_res[34] = str_replace('<>', "; ", $all_res[34]);
+        $all_res[35] = str_replace('<>', "; ", $all_res[35]);
+        $all_details[] = $all_res;
+        // All Details =========================================================================
     }
-    foreach ($pes_res as $pes_inner) {
+
+
+    foreach ($personal_details as $pes_inner) {
         $pes_list = array_values($pes_inner);
         $pes_details[] = $pes_list;
     }
-    // Personal Details ==================================================================
 
-    // Qualification Details ==================================================================
-    $ug_details_array = array();
-    $ugq = mysqli_query($con, "SELECT t1.* FROM `reg_dir` t0, `qualifications` t1 WHERE t0.`all_done` = 'tab7()' AND t1.ApplicationId = t0.ApplicationId AND t1.`qualification` = 'Under Graduation';");
-    while ($ug_details = mysqli_fetch_assoc($ugq)) {
-        // Remove Serial Number and Application ID from the array
-        unset($ug_details['sno']);
-        unset($ug_details['ApplicationId']);
-        $ug_details['year'] = str_replace("<>", "; ", $ug_details['year']);
-        $ug_details['specialization'] = str_replace("<>", "; ", $ug_details['specialization']);
-        $ug_details['university'] = str_replace("<>", "; ", $ug_details['university']);
-        $ug_res[] = $ug_details;
-    }
-    foreach ($ug_res as $ug_inner){
+    foreach ($ug_details as $ug_inner){
         $ug_list = array_values($ug_inner);
         $ug_details_array[] = $ug_list;
     }
 
-    $pg_details_array = array();
-    $pgq = mysqli_query($con, "SELECT t1.* FROM `reg_dir` t0, `qualifications` t1 WHERE t0.`all_done` = 'tab7()' AND t1.ApplicationId = t0.ApplicationId AND t1.`qualification` = 'Post Graduation';");
-    while ($pg_details = mysqli_fetch_assoc($pgq)) {
-        // Remove Serial Number and Application ID from the array
-        unset($pg_details['sno']);
-        unset($pg_details['ApplicationId']);
-        $pg_details['year'] = str_replace("<>", "; ", $pg_details['year']);
-        $pg_details['specialization'] = str_replace("<>", "; ", $pg_details['specialization']);
-        $pg_details['university'] = str_replace("<>", "; ", $pg_details['university']);
-        $pg_res[] = $pg_details;
-    }
-    foreach ($pg_res as $pg_inner){
+    foreach ($pg_details as $pg_inner){
         $pg_list = array_values($pg_inner);
         $pg_details_array[] = $pg_list;
     }
 
-    $phd_details_array = array();
-    $phdq = mysqli_query($con, "SELECT t1.* FROM `reg_dir` t0, `qualifications` t1 WHERE t0.`all_done` = 'tab7()' AND t1.ApplicationId = t0.ApplicationId AND t1.`qualification` = 'Ph.D';");
-    while ($phd_details = mysqli_fetch_assoc($phdq)) {
-        // Remove Serial Number and Application ID from the array
-        unset($phd_details['sno']);
-        unset($phd_details['ApplicationId']);
-        $phd_details['year'] = str_replace("<>", "; ", $phd_details['year']);
-        $phd_details['specialization'] = str_replace("<>", "; ", $phd_details['specialization']);
-        $phd_details['university'] = str_replace("<>", "; ", $phd_details['university']);
-        $phd_res[] = $phd_details;
-    }
-    foreach ($phd_res as $phd_inner){
+    foreach ($phd_details as $phd_inner){
         $phd_list = array_values($phd_inner);
         $phd_details_array[] = $phd_list;
     }
-    // Qualification Details ==================================================================
 
-    // Teaching Experience ==================================================================
-    $t_details_array = array();
-    $pq = mysqli_query($con, "SELECT T1.* FROM `reg_dir` T0, `teachingexp` T1 WHERE T0.`all_done` = 'tab7()' AND T0.ApplicationId = T1.ApplicationId;");
-    while ($teaching_exp = mysqli_fetch_assoc($pq)) {
-        // Remove Serial Number and Application ID from the array
-        unset($teaching_exp['sno']);
-        unset($teaching_exp['ApplicationId']);
-        $t_res[] = $teaching_exp;
-    }
-    foreach ($t_res as $t_inner){
+    foreach ($t_details as $t_inner){
         $t_list = array_values($t_inner);
         $t_details_array[] = $t_list;
     }
-    // Teaching Experience ==================================================================
 
-    // Working Experience ==================================================================
-    $p_details_array = array();
-    $profq = mysqli_query($con, "SELECT t1.* FROM `reg_dir` t0, `worked_universities` t1 WHERE t0.`all_done` = 'tab7()' AND t1.ApplicationId = t0.ApplicationId AND t1.`designation` = 'Professor';");
-    while ($prof_details = mysqli_fetch_assoc($profq)) {
-        // Remove Serial Number and Application ID from the array
-        unset($prof_details['sno']);
-        unset($prof_details['ApplicationId']);
-        $prof_details['dateOfAppointment'] = str_replace("*", "; ", $prof_details['dateOfAppointment']);
-        $prof_details['upto'] = str_replace("*", "; ", $prof_details['upto']);
-        $prof_details['university'] = str_replace("*", "; ", $prof_details['university']);
-        $prof_details['privateCollege'] = str_replace("*", "; ", $prof_details['privateCollege']);
-        $p_res[] = $prof_details;
-    }
-    foreach ($p_res as $p_inner){
+    foreach ($p_details as $p_inner){
         $p_list = array_values($p_inner);
         $p_details_array[] = $p_list;
     }
 
-    $a_details_array = array();
-    $assoq = mysqli_query($con, "SELECT t1.* FROM `reg_dir` t0, `worked_universities` t1 WHERE t0.`all_done` = 'tab7()' AND t1.ApplicationId = t0.ApplicationId AND t1.`designation` = 'Associate Professor';");
-    while ($asso_details = mysqli_fetch_assoc($assoq)) {
-        // Remove Serial Number and Application ID from the array
-        unset($asso_details['sno']);
-        unset($asso_details['ApplicationId']);
-        $asso_details['dateOfAppointment'] = str_replace("*", "; ", $asso_details['dateOfAppointment']);
-        $asso_details['upto'] = str_replace("*", "; ", $asso_details['upto']);
-        $asso_details['university'] = str_replace("*", "; ", $asso_details['university']);
-        $asso_details['privateCollege'] = str_replace("*", "; ", $asso_details['privateCollege']);
-        $a_res[] = $asso_details;
-    }
-    foreach ($a_res as $a_inner){
+    foreach ($a_details as $a_inner){
         $a_list = array_values($a_inner);
         $a_details_array[] = $a_list;
     }
 
-    $s_details_array = array();
-    $assiq = mysqli_query($con, "SELECT t1.* FROM `reg_dir` t0, `worked_universities` t1 WHERE t0.`all_done` = 'tab7()' AND t1.ApplicationId = t0.ApplicationId AND t1.`designation` = 'Assistant Professor';");
-    while ($assi_details = mysqli_fetch_assoc($assiq)) {
-        // Remove Serial Number and Application ID from the array
-        unset($assi_details['sno']);
-        unset($assi_details['ApplicationId']);
-        $assi_details['dateOfAppointment'] = str_replace("*", "; ", $assi_details['dateOfAppointment']);
-        $assi_details['upto'] = str_replace("*", "; ", $assi_details['upto']);
-        $assi_details['university'] = str_replace("*", "; ", $assi_details['university']);
-        $assi_details['privateCollege'] = str_replace("*", "; ", $assi_details['privateCollege']);
-        $s_res[] = $assi_details;
-    }
-    foreach ($s_res as $s_inner){
+    foreach ($s_details as $s_inner){
         $s_list = array_values($s_inner);
         $s_details_array[] = $s_list;
     }
-    // Working Experience ==================================================================
 
-    // All Details ==================================================================
-    $all_details_array = array();
-    $allq = mysqli_query($con, "SELECT T1.*, T2.*, T3.*, T4.*, T5.* FROM `reg_dir` T0, researchexp T1, administrativeexp T2, cases T3, other T4, applied_campus T5  WHERE T0.`all_done` = 'tab7()' AND T0.ApplicationId = T5.ApplicationId AND T1.ApplicationId = T5.ApplicationId AND T2.ApplicationId = T5.ApplicationId AND T3.ApplicationId = T5.ApplicationId AND T4.ApplicationId = T5.ApplicationId;");
-    while ($all_details = mysqli_fetch_assoc($allq)) {
-        // Remove Serial Number and Application ID from the array
-        unset($all_details['sno']);
-        unset($all_details['ApplicationId']);
-        $all_details["position"] = str_replace('<>', "; ", $all_details['position']);
-        $all_details["fromDate"] = str_replace('<>', "; ", $all_details['fromDate']);
-        $all_details["toDate"] = str_replace('<>', "; ", $all_details['toDate']);
-        $all_details["typeOf_case"] = str_replace('<>', "; ", $all_details['typeOf_case']);
-        $all_details["CaseName"] = str_replace('<>', "; ", $all_details['CaseName']);
-        $all_details["CaseStatus"] = str_replace('<>', "; ", $all_details['CaseStatus']);
-        $all_res[] = $all_details;
-    }
-    foreach ($all_res as $all_inner){
+    foreach ($all_details as $all_inner){
         $all_list = array_values($all_inner);
         $all_details_array[] = $all_list;
     }
-    // All Details ==================================================================
 
-    // Close the database connection
-    mysqli_close($con);
+//     // Close the database connection
+//     // mysqli_close($con);
 
-    // Combine all arrays into one global array
+//     // Combine all arrays into one global array
     for($i=0;$i<count($pes_details);$i++){
-        
         $merged_array = array_merge($pes_details[$i], $ug_details_array[$i], $pg_details_array[$i], $phd_details_array[$i], $t_details_array[$i], $p_details_array[$i], $a_details_array[$i], $s_details_array[$i], $all_details_array[$i]);
+        $merged_array[0] = $i+1;
         $global_array[] = $merged_array;
     };
-    // print_r($global_array);
+    // print_r($global_array); echo '<br><br><br>';
 
-    // Define the CSV file path
+//     // Define the CSV file path
     if ($path == '2811/1601'){
         $csvFilePath = 'C:/directorapp_data.csv';
         
@@ -183,12 +198,12 @@ if(isset($_GET['code']) && ($_GET['code'] =="1601/2811" || $_GET['code'] =="2811
         $csvFilePath = 'directorapp_data.csv';
     }
 
-    // Open the CSV file for writing
+//     // Open the CSV file for writing
     $csvFile = fopen($csvFilePath, 'w');
 
 
     $columnHeadings = array(
-        'S. No', 'Application Id', 'Name', 'Email Id', 'Mobile NUmber', 'Profile Picture', 'Date of Birth', 'Age', 'Gender', 'Social Status', 'Address', 'Present Designation', 'Department', 'Organisation Name',
+        'S. No', 'Application Id', 'Name', 'Email Id', 'Mobile Number', 'Profile Picture', 'Date of Birth', 'Age', 'Gender', 'Social Status', 'Address', 'Present Designation', 'Department', 'Organisation Name',
         'Qualification', 'Year of Completion (Under Graduation)', 'Specialization (Under Graduation)', 'University (Under Graduation)',
         'Qualification', 'Year of Completion (Post Graduation)', 'Specialization (Post Graduation)', 'University (Post Graduation)',
         'Qualification', 'Year of Completion (Ph.D)', 'Specialization (Ph.D)', 'University (Ph.D)',
@@ -204,22 +219,23 @@ if(isset($_GET['code']) && ($_GET['code'] =="1601/2811" || $_GET['code'] =="2811
         'Other Information', 'Applied Campus'
     );
 
-    // Open the CSV file for writing
+//     // Open the CSV file for writing
     $csvFile = fopen($csvFilePath, 'w');
 
-    // Write column headings as the first row in the CSV file
+
+//     // Write column headings as the first row in the CSV file
     fputcsv($csvFile, $columnHeadings);
 
 
-    // Iterate through the global array and write each row to the CSV file
+//     // Iterate through the global array and write each row to the CSV file
     foreach ($global_array as $row) {
         fputcsv($csvFile, $row);
     }
 
-    // Close the CSV file
+//     // Close the CSV file
     fclose($csvFile);
 
-    // Output a message to the user indicating the CSV file has been created
+//     // Output a message to the user indicating the CSV file has been created
     ?>
     <div class="container">
         <div class="row">
